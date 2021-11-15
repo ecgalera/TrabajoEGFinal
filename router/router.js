c3e97d1d99a867928997e8d5e8bc7cd7dcb4db3d
Eduardo Galera <galera.eduardo@gmail.com>
2021-11-15 16:29:00 -0300

    Casi listo

--- router/router.js
+++ router/router.js
@@ -44,7 +44,7 @@ router.post("/save", (req,res)=>{
     let sql = "INSERT INTO datosclientes SET ?";
     let query = connection.query(sql, data, (err, results)=>{
         if(err) throw err;
-        res.redirect("/");
+        res.redirect("/home");
     })
 
 
@@ -66,7 +66,7 @@ router.post('/update', (req, res) => {
     let sql = "UPDATE datosclientes SET nombre='" + req.body.nombre + "', apellido='" + req.body.apellido + "' , dni='" + req.body.dni + "', direccion='" + req.body.direccion + "', telefono='" + req.body.telefono + "', localidad='" + req.body.localidad + "', cp='" + req.body.cp + "', email='" + req.body.email + "', fn='" + req.body.fn + "' WHERE id=" + req.body.id;
     let query = connection.query(sql, (err, results) => {
         if (err) throw err;
-        res.redirect('/');
+        res.redirect('/editar');
     });
 });
 
@@ -75,7 +75,7 @@ router.post('/delete', (req, res) => {
     let sql = "DELETE FROM datosclientes WHERE id=" + req.body.id + "";
     let query = connection.query(sql, (err, results) => {
         if (err) throw err;
-        res.redirect('/');
+        res.redirect('/editar');
     });
 });
 
